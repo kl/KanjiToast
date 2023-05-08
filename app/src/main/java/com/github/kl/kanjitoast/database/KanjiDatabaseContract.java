df038593a31775c4fc7039874a4fa7b60ad9a9c2
removedporn <86824510+removedporn@users.noreply.github.com>
2023-05-08 11:32:52 +0800

    Delete app directory

--- app/src/main/java/com/github/kl/kanjitoast/database/KanjiDatabaseContract.java
+++
@@ -1,9 +0,0 @@
-package com.github.kl.kanjitoast.database;
-
-public interface KanjiDatabaseContract {
-
-    public static final String TABLE_NAME          = "KanjiKeywords";
-    public static final String KANJI_COLUMN_NAME   = "kanji";
-    public static final String KEYWORD_COLUMN_NAME = "keyword";
-
-}
